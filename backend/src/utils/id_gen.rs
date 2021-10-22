4d0e7f328392bc470bd5e0940c4fa1ee6bad6e2b
BobAnkh <bobankhshen@gmail.com>
2021-10-22 13:13:45 +0000

    chore(deps): bump idgenerator to latest version

--- backend/src/utils/id_gen.rs
+++ backend/src/utils/id_gen.rs
@@ -1,6 +1,7 @@
 use idgenerator::{IdHelper, IdGeneratorOptions};
 
 pub fn init(worker_id: u32) {
+    IdHelper::init();
     let mut options: IdGeneratorOptions = IdGeneratorOptions::new(worker_id);
     options.worker_id_bit_len = 8;
     IdHelper::set_id_generator(options);
