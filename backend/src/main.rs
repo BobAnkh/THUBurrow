97c3d11c9eaf89df1f96370f6b61ed7e1621428d
BobAnkh <bobankhshen@gmail.com>
2021-10-22 21:23:37 +0800

    feat(cors): add support for cors (#1)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,17 +4,20 @@ extern crate rocket;
 use rocket::fairing::AdHoc;
 use rocket_db_pools::Database;
 
-mod pool;
-mod routes;
-mod req;
+mod cors;
 mod db;
+mod pool;
+mod req;
+mod routes;
 
 use pool::Db;
 use routes::sample;
 
 #[launch]
 fn rocket() -> _ {
+    let cors_handler = cors::init();
     rocket::build()
+        .attach(cors_handler)
         .attach(Db::init())
         .attach(AdHoc::on_ignite("mount_user", sample::init))
 }
