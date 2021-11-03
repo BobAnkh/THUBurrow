51c4dd948a31acb1cb8f04186aab36a6d8886572
GH <h-guo18@foxmail.com>
2021-11-03 16:34:50 +0800

    add consumer

---
+++ backend/src/bin/consumer/main.rs
@@ -0,0 +1,77 @@
+#[macro_use]
+extern crate serde;
+use futures::TryStreamExt;
+use pulsar::{
+    Authentication, Consumer, DeserializeMessage, Payload, Pulsar, SubType, TokioExecutor,
+};
+use std::env;
+
+#[derive(Serialize, Deserialize)]
+struct TestData {
+    data: String,
+}
+
+impl DeserializeMessage for TestData {
+    type Output = Result<TestData, serde_json::Error>;
+
+    fn deserialize_message(payload: &Payload) -> Self::Output {
+        serde_json::from_slice(&payload.data)
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), pulsar::Error> {
+    env_logger::init();
+
+    let addr = env::var("PULSAR_ADDRESS")
+        .ok()
+        .unwrap_or_else(|| "pulsar://127.0.0.1:6650".to_string());
+    let topic = env::var("PULSAR_TOPIC")
+        .ok()
+        .unwrap_or_else(|| "persistent://public/default/search".to_string());
+
+    let mut builder = Pulsar::builder(addr, TokioExecutor);
+
+    if let Ok(token) = env::var("PULSAR_TOKEN") {
+        let authentication = Authentication {
+            name: "token".to_string(),
+            data: token.into_bytes(),
+        };
+
+        builder = builder.with_auth(authentication);
+    }
+
+    let pulsar: Pulsar<_> = builder.build().await?;
+
+    let mut consumer: Consumer<TestData, _> = pulsar
+        .consumer()
+        .with_topic(topic)
+        .with_consumer_name("test_consumer")
+        .with_subscription_type(SubType::Exclusive)
+        .with_subscription("test_subscription")
+        .build()
+        .await?;
+
+    let mut counter = 0usize;
+    while let Some(msg) = consumer.try_next().await? {
+        consumer.ack(&msg).await?;
+        log::info!("metadata: {:?}", msg.metadata());
+        log::info!("id: {:?}", msg.message_id());
+        let data = match msg.deserialize() {
+            Ok(data) => data,
+            Err(e) => {
+                log::error!("could not deserialize message: {:?}", e);
+                break;
+            }
+        };
+        println!("{}",data.data);
+        // if data.data.as_str() != "data" {
+        //     log::error!("Unexpected payload: {}", &data.data);
+        //     break;
+        // }
+        counter += 1;
+        log::info!("got {} messages", counter);
+    }
+
+    Ok(())
+}
