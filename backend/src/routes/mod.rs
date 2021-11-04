e7fb0bf8743e37498f06c43ece9ff749ca9db2ee
BobAnkh <bobankhshen@gmail.com>
2021-11-04 16:14:11 +0000

    feat(health): add health check
    Resolves: #6

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,1 +1,9 @@
+pub mod health;
 pub mod sample;
+
+use rocket::{fairing::AdHoc, Build, Rocket};
+
+pub async fn routes_init(rocket: Rocket<Build>) -> Rocket<Build> {
+    rocket
+        .attach(AdHoc::on_ignite("mount_health_check", health::init))
+}
