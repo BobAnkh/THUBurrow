bed18461663863a72882e103746b709ba379c478
BobAnkh <bobankhshen@gmail.com>
2021-11-07 02:12:15 +0800

    feature(storage): add image storage (#9)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,10 +6,21 @@ use rocket::{Build, Rocket};
 use rocket_db_pools::Database;
 
 use backend::cors;
-use backend::pool::{PgDb, RedisDb};
+use backend::pool::{MinioImageStorage, PgDb, RedisDb};
 use backend::routes::{self, sample};
 use backend::setup;
 use backend::utils::id_gen;
+
+#[cfg(debug_assertions)]
+fn log_init() {}
+
+#[cfg(not(debug_assertions))]
+fn log_init() {
+    match log4rs::init_file("conf/log4rs.yml", Default::default()) {
+        Ok(_) => (),
+        Err(e) => panic!("Error initial logger: {}", e),
+    }
+}
 
 async fn user_table_setup(rocket: Rocket<Build>) -> fairing::Result {
     let conn = &PgDb::fetch(&rocket).unwrap().connection;
@@ -19,11 +30,13 @@ async fn user_table_setup(rocket: Rocket<Build>) -> fairing::Result {
 
 #[launch]
 fn rocket() -> _ {
+    log_init();
     id_gen::init(1);
     let cors_handler = cors::init();
     rocket::build()
         .attach(PgDb::init())
         .attach(RedisDb::init())
+        .attach(MinioImageStorage::init())
         .attach(cors_handler)
         .attach(AdHoc::on_ignite("mount_routes", routes::routes_init))
         .attach(AdHoc::on_ignite("mount_user", sample::init))
--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,5 +1,6 @@
 pub mod health;
 pub mod sample;
+pub mod storage;
 pub mod user;
 
 use rocket::{fairing::AdHoc, Build, Rocket};
@@ -8,4 +9,5 @@ pub async fn routes_init(rocket: Rocket<Build>) -> Rocket<Build> {
     rocket
         .attach(AdHoc::on_ignite("mount_health_check", health::init))
         .attach(AdHoc::on_ignite("mount_user", user::init))
+        .attach(AdHoc::on_ignite("mount_storage", storage::init))
 }
