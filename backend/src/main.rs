145d96f8a61630edeb8e4b8353659ec8f99d278d
ggbot123 <91649311+ggbot123@users.noreply.github.com>
2021-12-03 00:49:34 +0800

    feat(burrow): implement core concept burrow (#20)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,41 +1,7 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::fairing::AdHoc;
-use rocket_db_pools::Database;
-
-use backend::pool::{MinioImageStorage, PgDb, RedisDb};
-use backend::routes::{self, sample};
-use backend::setup;
-use backend::utils::{cors, id_gen};
-
-#[cfg(debug_assertions)]
-fn log_init() {}
-
-#[cfg(not(debug_assertions))]
-fn log_init() {
-    let filename = if std::path::Path::new("/etc/backend/conf/log4rs.yml").exists() {
-        "/etc/backend/conf/log4rs.yml"
-    } else {
-        "/etc/backend/conf/log4rs-default.yml"
-    };
-    match log4rs::init_file(filename, Default::default()) {
-        Ok(_) => (),
-        Err(e) => panic!("Error initial logger: {}", e),
-    }
-}
-
 #[launch]
 fn rocket() -> _ {
-    log_init();
-    id_gen::init(1);
-    let cors_handler = cors::init();
-    rocket::build()
-        .attach(cors_handler)
-        .attach(PgDb::init())
-        .attach(RedisDb::init())
-        .attach(MinioImageStorage::init())
-        .attach(AdHoc::on_ignite("mount_user", sample::init))
-        .attach(AdHoc::on_ignite("mount_routes", routes::routes_init))
-        .attach(AdHoc::try_on_ignite("Migrations", setup::user_table_setup))
+    backend::rocket_init()
 }
--- backend/src/utils/mod.rs
+++ backend/src/utils/mod.rs
@@ -1,4 +1,5 @@
+pub mod auth;
+pub mod burrow_valid;
 pub mod cors;
 pub mod email;
 pub mod id_gen;
-pub mod sso;
