7e9a75d66ae23faa0c79a1ae8274863d001cabc7
BobAnkh <bobankhshen@gmail.com>
2021-12-06 04:14:26 +0000

    add aync test

--- backend/tests/common/mod.rs
+++ backend/tests/common/mod.rs
@@ -1,12 +1,33 @@
-use once_cell::sync::OnceCell;
+use backend::utils::mq::*;
+// use once_cell::sync::OnceCell;
 use parking_lot::Mutex;
-use rocket::local::blocking::Client;
+// use rocket::local::blocking::Client;
 
-pub fn get_client() -> &'static Mutex<Client> {
-    static INSTANCE: OnceCell<Mutex<Client>> = OnceCell::new();
-    INSTANCE.get_or_init(|| {
-        let rocket = backend::rocket_init();
-        let client = Client::tracked(rocket).expect("valid rocket instance");
-        Mutex::new(client)
-    })
+// pub fn get_client() -> &'static Mutex<Client> {
+//     static INSTANCE: OnceCell<Mutex<Client>> = OnceCell::new();
+//     INSTANCE.get_or_init(|| {
+//         let rocket = backend::rocket_init();
+//         let client = Client::tracked(rocket).expect("valid rocket instance");
+//         Mutex::new(client)
+//     })
+// }
+
+use rocket::local::asynchronous::Client;
+use tokio::sync::OnceCell;
+
+pub async fn get_client() -> &'static Mutex<Client> {
+    static INSTANCE: OnceCell<Mutex<Client>> = OnceCell::const_new();
+    INSTANCE
+        .get_or_init(|| async {
+            let scheduler = vec![tokio::spawn(generate_trending())];
+            let handles = vec![
+                tokio::spawn(pulsar_relation()),
+                tokio::spawn(pulsar_typesense()),
+            ];
+            let rocket = backend::rocket_init();
+            let client = Client::tracked(rocket).await.expect("valid rocket instance");
+            let r = Mutex::new(client);
+            r
+        })
+        .await
 }
