89030b09691cbb58623c1ec1b8c72db2b7410b34
taozhan <853142213@qq.om>
2021-12-18 21:40:41 +0800

    retrieve-password

--- frontend/components/findback-password.tsx
+++ frontend/components/findback-password.tsx
@@ -2,7 +2,11 @@ import React, { useState } from 'react';
 import 'antd/dist/antd.css';
 import styles from '../styles/register.module.css';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import { Form, Input, Row, Col, Button } from 'antd';
+import { Form, Input, Row, Col, Button, message, Select } from 'antd';
+import axios, { AxiosError } from 'axios';
+import CryptoJS from 'crypto-js';
+
+const { Option } = Select;
 
 type Iprops = {
   switchform: any;
@@ -11,13 +15,42 @@ type Iprops = {
 const validate_password = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z-_]{6,20}$/;
 
 export default function FindbackPassword({ switchform }: Iprops) {
-  async function onFinish(values: any) {}
   const [btnText, setbtnText] = useState('发送验证码');
   const [btnBool, setbtnBool] = useState(false);
+  const [email, setEmail] = useState('');
+  const [suffix, setSuffix] = useState('@mails.tsinghua.edu.cn');
   const toggleForm = () => {
     switchform('login');
   };
-  function SendCode() {
+
+  async function onFinish(values: any) {
+    const data = {
+      email: email + suffix,
+      password: CryptoJS.MD5(values.password).toString(),
+      verification_code: values.code,
+    };
+    axios
+      .post(`${process.env.NEXT_PUBLIC_BASEURL}/users/reset`, data)
+      .then(function (res) {
+        message.success('重置成功');
+        window.location.href = '../login';
+      })
+      .catch(function (e) {
+        const err = e as AxiosError;
+        if (err.response?.status == 400) {
+          message.error(err.response.data.code);
+        }
+        if (err.response?.status == 429) {
+          message.error('请求过于频繁');
+        }
+        if (err.response?.status == 500) {
+          message.error('后端内部错误');
+        }
+        message.error('找回密码失败');
+      });
+  }
+
+  async function SendCode() {
     let maxTime = 60;
     const timer = setInterval(() => {
       if (maxTime > 0) {
@@ -30,7 +63,50 @@ export default function FindbackPassword({ switchform }: Iprops) {
         clearInterval(timer);
       }
     }, 1000);
+    const data = {
+      email: email + suffix,
+    };
+    axios
+      .post(`${process.env.NEXT_PUBLIC_BASEURL}/users/reset/email`, data)
+      .then(function (res) {
+        message.success('发送验证码成功');
+      })
+      .catch(function (e) {
+        const err = e as AxiosError;
+        if (err.response?.status == 400) {
+          message.error('邮箱格式错误或尚未注册过');
+        }
+        if (err.response?.status == 429) {
+          message.error('请求次数过多或邮箱不存在');
+        }
+        if (err.response?.status == 500) {
+          message.error('后端内部错误');
+        }
+        message.error('发送验证码失败');
+      });
   }
+
+  function handleEmail(event: any) {
+    if (event && event.target && event.target.value) {
+      let value = event.target.value;
+      setEmail(value);
+    }
+  }
+
+  function handleSuffix(value: any) {
+    setSuffix(value);
+  }
+
+  const selectAfter = (
+    <Select
+      defaultValue='@mails.tsinghua.edu.cn'
+      onChange={handleSuffix}
+      className={styles.select_after}
+    >
+      <Option value='@pku.edu.cn'>@pku.edu.cn</Option>
+      <Option value='@mails.tsinghua.edu.cn'>@mails.tsinghua.edu.cn</Option>
+    </Select>
+  );
   return (
     <div className={styles.background}>
       <title>找回密码</title>
@@ -51,19 +127,23 @@ export default function FindbackPassword({ switchform }: Iprops) {
               </span>
             </Form.Item>
             <Form.Item
-              name='username'
+              name='email'
               rules={[
                 {
-                  type: 'email',
-                  message: '邮箱格式不正确',
+                  required: true,
+                  message: 'Please input a valid email address!',
                 },
                 {
-                  required: true,
-                  message: '请输入你的邮箱!',
+                  pattern: /^[0-9a-z-A-Z-]{1,}$/,
+                  message:
+                    "the address should only contains letters, numbers and '-'",
                 },
               ]}
+              className={styles.formStyle}
             >
               <Input
+                addonAfter={selectAfter}
+                onChange={(event) => handleEmail(event)}
                 prefix={<UserOutlined className='site-form-item-icon' />}
                 placeholder='请输入你的邮箱:'
               />
