fe6f1e8429bdb1e2e65ae48fe01604995d1a7551
BobAnkh <41898282+github-actions[bot]@users.noreply.github.com>
2021-12-19 04:38:48 +0000

    docs(backend): update api documentations

---
+++ implementors/task_executor/trait.Typesense.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["task_executor"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
