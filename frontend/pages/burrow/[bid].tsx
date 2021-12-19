5e85a4bbcc9aaf1cc5752f08719e54ef5120fecc
lcf0428 <m13810660382@163.com>
2021-12-19 13:15:36 +0800

    remove the older version

--- frontend/pages/burrow/[bid].tsx
+++ frontend/pages/burrow/[bid].tsx
@@ -227,12 +227,6 @@ const Burrow: NextPage = () => {
         className='site-Layout'
         style={{ padding: '0 50px', marginTop: 64 }}
       >
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
-
         <div
           className='site-layout-background'
           style={{ padding: 24, minHeight: 380 }}
