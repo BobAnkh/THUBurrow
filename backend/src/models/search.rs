ecc87b4196d3234887082bf70f6772506a23a06c
GH <h-guo18@foxmail.com>
2021-12-20 15:23:58 +0800

    add docs for models/search.rs

--- backend/src/models/search.rs
+++ backend/src/models/search.rs
@@ -67,16 +67,15 @@ pub struct TypesenseReplyData {
     pub update_time: DateTimeWithTimeZone,
 }
 
-/// Different kinds of earch request
+/// Different kinds of search request
 ///
 /// ## enum
 ///
-/// - `id`: Reply id in String for searchengine index
-/// - `reply_id`: Reply id in i64
-/// - `post_id`: i64 id of post to which the reply belongs
-/// - `burrow_id`: i64 id of burrow to which the post belongs
-/// - `content`: Reply content in String
-/// - `update_time`: Update time in DataTimeWithTimeZone struct
+/// - `SearchRequest::RetrieveBurrow`: Retrieve burrow with param `burrow_id` in i64
+/// - `SearchRequest::RetrievePost`: Retrieve post with param `post_id` in i64
+/// - `SearchRequest::SearchBurrowKeyword`: Search burrow with param `keywords` in vector of String
+/// - `SearchRequest::SearchPostKeyword`: Search post with param `keywords` in vector of String
+/// - `SearchRequest::SearchPostTag`: Search post with param `tag` in vector of String
 ///
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub enum SearchRequest {
@@ -87,6 +86,14 @@ pub enum SearchRequest {
     SearchPostTag { tag: Vec<String> },
 }
 
+/// Response struct for burrow search
+///
+/// ## Fields
+///
+/// - `found`: Number of found burrow in i32
+/// - `page`: Page index of result
+/// - `burrows`: Vector of search result in struct PulsarSearchBurrowData
+///
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct SearchBurrowResponse {
     pub found: i32,
@@ -94,6 +101,14 @@ pub struct SearchBurrowResponse {
     pub burrows: Vec<PulsarSearchBurrowData>,
 }
 
+/// Search result for burrow search
+///
+/// ## Fields
+///
+/// - `found`: Number of found burrow in i32
+/// - `page`: Page index of result
+/// - `hits`: Vector of search hit in struct SearchBurrowHit
+///
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct SearchBurrowData {
     pub found: i32,
@@ -101,29 +116,64 @@ pub struct SearchBurrowData {
     pub hits: Vec<SearchBurrowHit>,
 }
 
+/// Search hit for burrow
+///
+/// ## Fields
+///
+/// - `highlight`: Vector of SeachHighlight
+/// - `document`: Burrow data in struct TypesenseBurrowData
+///
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct SearchBurrowHit {
     pub highlights: Vec<SearchHighlight>,
     pub document: TypesenseBurrowData,
 }
 
+/// Search highlight
+///
+/// ## Fields
+///
+/// - `field`:  The field that the keyword hit in String
+/// - `snippet`: A slice of context around hitword in String
+///
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct SearchHighlight {
     pub field: String,
     pub snippet: String,
 }
 
+/// Response struct for post/reply mix search
+///
+/// ## Fields
+///
+/// - `post`: Response for post search
+/// - `replies`: Response for reply seach
+///
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct SearchMixResponse {
     pub posts: SearchPostResponse,
     pub replies: SearchReplyResponse,
 }
 
+/// Search result for post/reply mix search
+///
+/// ## Fields
+///
+/// - `results`: A tuple (SeachPostData, SeachReplyData)
+///
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct SearchMixResult {
     pub results: (SearchPostData, SearchReplyData),
 }
 
+/// Response struct for post search
+///
+/// ## Fields
+///
+/// - `found`: Number of found in i32
+/// - `page`: Page index of result
+/// - `burrows`: Vector of search result in struct PulsarSearchPostData
+///
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct SearchPostResponse {
     pub found: i32,
@@ -131,6 +181,14 @@ pub struct SearchPostResponse {
     pub posts: Vec<PulsarSearchPostData>,
 }
 
+/// Search result for post search
+///
+/// ## Fields
+///
+/// - `found`: Number of found in i32
+/// - `page`: Page index of result
+/// - `hits`: Vector of search hit in struct SearchPostHit
+///
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct SearchPostData {
     pub found: i32,
@@ -138,12 +196,27 @@ pub struct SearchPostData {
     pub hits: Vec<SearchPostHit>,
 }
 
+/// Search hit for post
+///
+/// ## Fields
+///
+/// - `highlight`: Vector of SeachHighlight
+/// - `document`: Post data in struct TypesenseBurrowData
+///
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct SearchPostHit {
     pub highlights: Vec<SearchHighlight>,
     pub document: TypesensePostData,
 }
 
+/// Response struct for reply search
+///
+/// ## Fields
+///
+/// - `found`: Number of found in i32
+/// - `page`: Page index of result
+/// - `burrows`: Vector of search result in struct SearchReplyGroupResponse
+///
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct SearchReplyResponse {
     pub found: i32,
@@ -151,6 +224,14 @@ pub struct SearchReplyResponse {
     pub replies: Vec<SearchReplyGroupResponse>,
 }
 
+/// Search result for reply search
+///
+/// ## Fields
+///
+/// - `found`: Number of found in i32
+/// - `page`: Page index of result
+/// - `grouped_hits`: Vector of search hit in struct SearchReplyGroupHit
+///
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct SearchReplyData {
     pub found: i32,
@@ -158,24 +239,57 @@ pub struct SearchReplyData {
     pub grouped_hits: Vec<SearchReplyGroupHit>,
 }
 
+/// Grouped search result of reply from the same post
+///
+/// ## Fields
+///
+/// - `post_id`: Post id in i64
+/// - `replies`: Vector of hit reply in struct PulsarSearchReplyData
+///
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct SearchReplyGroupResponse {
     pub post_id: i64,
     pub replies: Vec<PulsarSearchReplyData>,
 }
 
+/// Grouped search hit for reply
+///
+/// ## Fields
+///
+/// - `goup_key`: Vector of index in i64
+/// - `hits`: Vector of hit reply in struct SearchReplyHit
+///
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct SearchReplyGroupHit {
     pub group_key: Vec<i64>,
     pub hits: Vec<SearchReplyHit>,
 }
 
+/// Grouped hit of single reply
+///
+/// ## Fields
+///
+/// - `highlight`: Vector of SeachHighlight
+/// - `document`: Reply data in struct TypesenseReplyData
+///
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct SearchReplyHit {
     pub highlights: Vec<SearchHighlight>,
     pub document: TypesenseReplyData,
 }
 
+/// Search parameters
+///
+/// ## Fields
+///
+/// - `collection`: The collection to search in String, burrows/posts/replies
+/// - `q`: Search keyword in String
+/// - `query_by`: The field to query
+/// - `filter_by`: Filter condition in String
+/// - `sort_by`: Sort result condition in String
+/// - `group_by`: Group condition in String
+/// - `highlight_fields`: Highlight fields in String
+///
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct SearchParam {
     pub collection: String,
@@ -187,6 +301,12 @@ pub struct SearchParam {
     pub highlight_fields: String,
 }
 
+/// A vector of search parameters
+///
+/// ## Fields
+///
+/// - `searches`: Vector of struct SearchParam
+///
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct MultiSearch {
     pub searches: Vec<SearchParam>,
