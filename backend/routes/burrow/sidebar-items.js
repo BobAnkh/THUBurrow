3e0f9bf5544eb92c21eb320c517d74b57655bc6b
BobAnkh <41898282+github-actions[bot]@users.noreply.github.com>
2021-12-20 12:30:10 +0000

    docs(backend): update api documentations

--- backend/routes/burrow/sidebar-items.js
+++ backend/routes/burrow/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["create_burrow","Create Burrow"],["discard_burrow","Discard Burrow"],["get_total_burrow_count",""],["init",""],["show_burrow","Show a Specific Burrow with Up to Ten Posts"],["update_burrow","Update Burrow"]]});
+initSidebarItems({"fn":[["create_burrow","Create Burrow"],["discard_burrow","Discard Burrow"],["get_total_burrow_count","Get total burrow count"],["init",""],["show_burrow","Show a Specific Burrow with Up to Ten Posts"],["update_burrow","Update Burrow"]]});
