d94d6670ac5ab1180b88cfe4a8f49658f93a4cd2
taozhan <853142213@qq.om>
2021-12-24 11:49:32 +0800

    set reply number

--- frontend/pages/searchpage.tsx
+++ frontend/pages/searchpage.tsx
@@ -350,8 +350,8 @@ const SearchPage: FC = () => {
             allowClear
             onSearch={handleFormSubmit}
           />
-          <p style={{ fontSize: '9px', opacity: 0.5 }}>
-            可根据关键词可以搜洞或帖，#tag搜对应标签帖子，#帖号进入帖,$洞号进入洞,可用空格分割关键词、tag搜索多个关键词、tag。
+          <p style={{ fontSize: '14px', opacity: 0.5 }}>
+            可根据关键词可以搜洞或帖，#tag搜对应标签帖子，#帖号进入帖，$洞号进入洞，可用空格分割关键词、tag搜索多个关键词、tag。
           </p>
         </div>
 
@@ -373,8 +373,14 @@ const SearchPage: FC = () => {
           {jump == false && (
             <p>
               找到
-              <mark>{showreply === false ? found_number : found_number1}</mark>
-              个结果
+              <mark>{found_number}</mark>个{state == 'burrow' ? '洞' : '帖子'}
+            </p>
+          )}
+          {jump == false && state == 'post' && search_text.tag[0] == null && (
+            <p>
+              找到
+              <mark>{found_number1}</mark>
+              个回复
             </p>
           )}
           <Card>
