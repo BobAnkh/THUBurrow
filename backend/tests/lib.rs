e433e51cb39c2390e871d001cca6189cbc12412f
ggbot123 <91649311+ggbot123@users.noreply.github.com>
2021-12-24 13:36:27 +0800

    Update lib.rs

--- backend/tests/lib.rs
+++ backend/tests/lib.rs
@@ -1098,7 +1098,7 @@ fn test_burrow() {
     assert_eq!(response.status(), Status::Ok);
     assert_eq!(
         response.into_string().unwrap(),
-        "{{\"title\":\"默认洞\",\"description\":\"Hello World!\",\"posts\":[]}}".to_string()
+        "{\"title\":\"默认洞\",\"description\":\"Hello World!\",\"posts\":[]}".to_string()
     );
     // show burrow: perform a wrong action (burrow not exist)
     let response = client
